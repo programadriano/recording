5fd01b33f2ec684419279948ce89c11a14345827
Thiago S Adriano <tadriano.net@gmail.com>
2023-02-10 22:36:00 -0300

    subindo ajustes

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+#region [Healthcheck]
+builder.Services.AddHealthChecks()
+            .AddNpgSql(builder.Configuration.GetSection("DatabaseSettings:ConnectionString").Value,
+                name: "postgreSQL", tags: new string[] { "db", "data" });
+
+
+
+builder.Services.AddHealthChecksUI(opt =>
+{
+    opt.SetEvaluationTimeInSeconds(15); //time in seconds between check
+    opt.MaximumHistoryEntriesPerEndpoint(60); //maximum history of checks
+    opt.SetApiMaxActiveRequests(1); //api requests concurrency
+
+    opt.AddHealthCheckEndpoint("default api", "/health"); //map health check api
+}).AddInMemoryStorage();
+
+#endregion
 
 var app = builder.Build();
 
@@ -21,6 +44,16 @@ if (app.Environment.IsDevelopment())
         c.SpecUrl = "/swagger/v1/swagger.json";
     });
 }
+
+#region [Healthcheck]
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+
+}).UseHealthChecksUI(h => h.UIPath = "/health-ui");
+
+#endregion
 
 app.UseAuthorization();
 
